f6650c626a376f3701f3496a493dbb808ccff782
Javeria Saeed <janeriasaeed102@gmail.com>
2020-02-09 21:16:00 +0500

    working on GetStarted

--- app/src/main/java/com/example/servicesondemand/director/Helpers.java
+++ app/src/main/java/com/example/servicesondemand/director/Helpers.java
@@ -20,6 +20,7 @@ public class Helpers {
     }
 
 
+
     public void showError(Activity activity,String title, String message){
         MaterialDialog mDialog = new MaterialDialog.Builder(activity)
                 .setTitle(title)
