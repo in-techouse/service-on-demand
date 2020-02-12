a8fa4597b260bd1cdbd5011c385b0a7017a14c07
janeriasaeed <janeriasaeed102@gmail.com>
2020-02-12 14:46:48 +0500

    working on GetStarted

--- app/src/main/java/com/example/servicesondemand/activities/CreatePost.java
+++ app/src/main/java/com/example/servicesondemand/activities/CreatePost.java
@@ -1,15 +1,11 @@
 package com.example.servicesondemand.activities;
 
 import android.os.Bundle;
-
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
 import android.view.View;
-
 import com.example.servicesondemand.R;
 
 public class CreatePost extends AppCompatActivity {
