86fc6ba8881707c19b845e38c98b35ef9bde01e5
Muhammad Taimoor Sultani <team.dev.man@gmail.com>
2020-02-25 14:40:39 +0500

    Splash Screen Changed

--- app/src/main/java/com/example/servicesondemand/activities/MainActivity.java
+++ app/src/main/java/com/example/servicesondemand/activities/MainActivity.java
@@ -1,27 +1,27 @@
 package com.example.servicesondemand.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
 import com.example.servicesondemand.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
-    private static int SPLASH_TIME_OUT = 3000;
+    private static int SPLASH_TIME_OUT = 1900;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new Handler().postDelayed(new Runnable(){
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void run(){
+            public void run() {
                 Intent homeIntent = new Intent(MainActivity.this, GetStarted.class);
                 startActivity(homeIntent);
                 finish();
             }
-        },SPLASH_TIME_OUT);
+        }, SPLASH_TIME_OUT);
     }
 }
